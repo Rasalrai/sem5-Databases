44e7d4a9580be77fc3432028442edcdc1aa7fec5
Julia <jula.stachowiak@gmail.com>
2020-10-20 19:07:32 +0200

    lab 2

--- labs/lab2.sql
+++ labs/lab2.sql
@@ -18,5 +18,118 @@ BEGIN
   );
 END NowyPracownik;
 
+
 -- zad 2
+
+CREATE FUNCTION PlacaNetto
+	(placa_brutto IN NUMBER,
+	podatek IN NUMBER DEFAULT 20)
+	RETURN NUMBER IS
+	placa_netto NUMBER;
+BEGIN
+  RETURN placa_brutto * ((100-podatek)/100);
+END PlacaNetto;
+
+
+-- zad 3
+
+CREATE OR REPLACE FUNCTION Silnia
+	(liczba IN NATURAL)
+	RETURN NATURAL IS
+	silnia NATURAL;
+BEGIN
+  silnia := 1;
+  FOR i IN 2..liczba LOOP
+    silnia := silnia * i;
+  END LOOP;
+  RETURN silnia;
+END Silnia;
+
+
+-- zad 4
+
+CREATE OR REPLACE FUNCTION SilniaRek
+	(liczba IN NATURAL)
+	RETURN NATURAL IS
+BEGIN
+  IF liczba < 2 THEN
+    RETURN 1;
+  END IF;
+  RETURN SilniaRek(liczba - 1) * liczba;
+END SilniaRek;
+
+-- zad 5
+
+CREATE OR REPLACE FUNCTION IleLat
+	(data_pocz IN DATE)
+	RETURN NATURAL IS
+BEGIN
+  RETURN TRUNC(MONTHS_BETWEEN(current_date, data_pocz)/12);
+END IleLat;
+
+
+-- zad 6
+
+CREATE OR REPLACE PACKAGE Konwersja IS
+	FUNCTION Cels_To_Fahr
+		(cels NUMBER)
+		RETURN NUMBER;
+
+	FUNCTION Fahr_To_Cels
+		(fahr NUMBER)
+		RETURN NUMBER;
+END Konwersja;
+
+CREATE OR REPLACE PACKAGE Konwersja IS
+	FUNCTION Cels_To_Fahr
+		(cels NUMBER)
+		RETURN NUMBER;
+
+	FUNCTION Fahr_To_Cels
+		(fahr NUMBER)
+		RETURN NUMBER;
+END Konwersja;
+
+
+-- zad 7
+
+CREATE OR REPLACE PACKAGE Zmienne IS
+	PROCEDURE ZwiekszLicznik;
+	PROCEDURE ZmniejszLicznik;
+	
+	FUNCTION PokazLicznik
+		RETURN NUMBER;
+
+END Zmienne;
+
+
+
+CREATE OR REPLACE PACKAGE BODY Zmienne IS
+	vLicznik NUMBER;
+
+	PROCEDURE ZwiekszLicznik IS
+	BEGIN
+      vLicznik := vLicznik + 1;
+      DBMS_OUTPUT.PUT_LINE('Zwiększono');
+	END ZwiekszLicznik;
+
+	PROCEDURE ZmniejszLicznik IS
+	BEGIN
+      vLicznik := vLicznik - 1;
+      DBMS_OUTPUT.PUT_LINE('Zmniejszono');
+	END ZmniejszLicznik;
+
+	FUNCTION PokazLicznik
+		RETURN NUMBER IS
+	BEGIN
+      RETURN vLicznik;
+    END PokazLicznik;
+
+	BEGIN
+		vLicznik := 1;
+    	DBMS_OUTPUT.PUT_LINE('Zainicjalizowano');
+END Zmienne;
+
+
+
 
