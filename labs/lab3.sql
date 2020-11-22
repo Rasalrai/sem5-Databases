c32d87ce2b0b4ea27ae5e5ae61988687233c2971
Julia <jula.stachowiak@gmail.com>
2020-11-22 21:48:34 +0100

    lab 3

--- labs/lab3.sql
+++ labs/lab3.sql
@@ -132,7 +132,7 @@ CREATE OR REPLACE PROCEDURE RaportKadrowy IS
   placa_etatu pracownicy.placa_pod%TYPE;
 
   CURSOR etaty IS
-	SELECT DISTINCT etat FROM pracownicy;
+	SELECT nazwa FROM etaty;
 
   CURSOR pracownicyEtatu(e pracownicy.etat%TYPE) IS
     SELECT nazwisko, placa_pod, placa_dod FROM pracownicy
@@ -143,10 +143,10 @@ BEGIN
   FOR e in etaty LOOP
     placa_etatu := 0;
     n_pracownikow := 0;
-    DBMS_OUTPUT.PUT_LINE('Etat: ' || e.etat);
+    DBMS_OUTPUT.PUT_LINE('Etat: ' || e.nazwa);
     DBMS_OUTPUT.PUT_LINE('------------------------------');
-    FOR osoba IN pracownicyEtatu(e.etat) LOOP
-      DBMS_OUTPUT.PUT_LINE(osoba.nazwisko || 'pensja: ' || osoba.placa_pod ); -- + NVL(osoba.placa_dod, 0));
+    FOR osoba IN pracownicyEtatu(e.nazwa) LOOP
+      DBMS_OUTPUT.PUT_LINE(pracownicyEtatu%ROWCOUNT || '. ' || osoba.nazwisko || ', pensja: ' || osoba.placa_pod ); -- + NVL(osoba.placa_dod, 0));
       placa_etatu := placa_etatu + osoba.placa_pod; -- + NVL(osoba.placa_dod, 0);
       n_pracownikow := n_pracownikow + 1;
     END LOOP;
@@ -155,12 +155,224 @@ BEGIN
     IF n_pracownikow = 0 THEN
       DBMS_OUTPUT.PUT_LINE('Średnia pensja: brak');
     ELSE
-      DBMS_OUTPUT.PUT_LINE('Średnia pensja: ' || placa_etatu);
+      DBMS_OUTPUT.PUT_LINE('Średnia pensja: ' || ROUND(placa_etatu/n_pracownikow, 2));
     END IF;
+    DBMS_OUTPUT.PUT_LINE('');
   END LOOP;
 END RaportKadrowy;
 
 
 BEGIN
   RaportKadrowy;
+END;
+
+
+-- zad 7
+
+
+CREATE OR REPLACE PACKAGE BODY IntZespoly IS
+    PROCEDURE addTeam(id zespoly.id_zesp%TYPE,
+				nzw zespoly.nazwa%TYPE,
+				adr zespoly.adres%TYPE) IS
+	BEGIN
+      INSERT INTO zespoly(id_zesp, nazwa, adres)
+      	VALUES(id, nzw, adr);
+
+      IF SQL%NOTFOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Nie dodano żadnego rekordu.');
+      END IF;
+    END addTeam;
+
+
+    PROCEDURE removeId(id zespoly.id_zesp%TYPE) IS
+	BEGIN
+      DELETE FROM zespoly WHERE id_zesp = id;
+      
+      IF SQL%NOTFOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Nie usunięto żadnego rekordu.');
+      END IF;
+    END removeId;
+
+
+	PROCEDURE removeName(nzw zespoly.nazwa%TYPE) IS
+	BEGIN
+      DELETE FROM zespoly WHERE nazwa = nzw;
+      
+      IF SQL%NOTFOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Nie usunięto żadnego rekordu.');
+      END IF;
+    END removeName;
+
+
+	PROCEDURE changeData(id zespoly.id_zesp%TYPE,
+				nzw zespoly.nazwa%TYPE,
+				adr zespoly.adres%TYPE) IS
+	BEGIN
+      UPDATE zespoly
+      SET nazwa = nzw,
+      	adres = adr
+      WHERE id_zesp = id;
+      
+      IF SQL%NOTFOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Nie zmieniono żadnego rekordu.');
+      END IF;
+    END changeData;
+
+
+	FUNCTION getId(nzw zespoly.nazwa%TYPE)
+		RETURN zespoly.id_zesp%TYPE IS
+		ret zespoly.id_zesp%TYPE;
+	BEGIN
+      SELECT id_zesp INTO ret
+      	FROM zespoly WHERE nazwa = nzw;
+      RETURN ret;
+    END getId;
+
+
+    FUNCTION getName(id zespoly.id_zesp%TYPE)
+		RETURN zespoly.nazwa%TYPE IS
+		ret zespoly.nazwa%TYPE;
+	BEGIN
+      SELECT nazwa INTO ret
+      	FROM zespoly WHERE id_zesp = id;
+      RETURN ret;
+    END getName;
+
+
+    FUNCTION getAddress(id zespoly.id_zesp%TYPE)
+		RETURN zespoly.adres%TYPE IS
+		ret zespoly.adres%TYPE;
+	BEGIN
+      SELECT adres INTO ret
+      	FROM zespoly WHERE id_zesp = id;
+      RETURN ret;
+    END getAddress;
+END IntZespoly;
+
+
+
+BEGIN
+  IntZespoly.removeName('Lorem ipsum');
+END;
+-- Nie usunięto żadnego rekordu.
+
+
+-- zad 8
+
+CREATE OR REPLACE PACKAGE IntZespoly IS
+    PROCEDURE addTeam(id zespoly.id_zesp%TYPE,
+				nzw zespoly.nazwa%TYPE,
+				adr zespoly.adres%TYPE);
+    PROCEDURE removeId(id zespoly.id_zesp%TYPE);
+	PROCEDURE removeName(nzw zespoly.nazwa%TYPE);
+	PROCEDURE changeData(id zespoly.id_zesp%TYPE,
+				nzw zespoly.nazwa%TYPE,
+				adr zespoly.adres%TYPE);
+	FUNCTION getId(nzw zespoly.nazwa%TYPE) RETURN zespoly.id_zesp%TYPE;
+    FUNCTION getName(id zespoly.id_zesp%TYPE) RETURN zespoly.nazwa%TYPE;
+    FUNCTION getAddress(id zespoly.id_zesp%TYPE) RETURN zespoly.adres%TYPE;
+	IDNotFoundEx EXCEPTION;
+	nameNotFoundEx EXCEPTION;
+	existingIdEx EXCEPTION;
+END IntZespoly;
+
+CREATE OR REPLACE PACKAGE BODY IntZespoly IS
+    PROCEDURE addTeam(id zespoly.id_zesp%TYPE,
+				nzw zespoly.nazwa%TYPE,
+				adr zespoly.adres%TYPE) IS
+	BEGIN
+      INSERT INTO zespoly(id_zesp, nazwa, adres)
+      	VALUES(id, nzw, adr);
+
+      IF SQL%NOTFOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Nie dodano żadnego rekordu.');
+      END IF;
+
+    EXCEPTION
+      WHEN DUP_VAL_ON_INDEX THEN
+        RAISE existingIdEx;
+    END addTeam;
+
+    PROCEDURE removeId(id zespoly.id_zesp%TYPE) IS
+	BEGIN
+      DELETE FROM zespoly WHERE id_zesp = id;
+      
+      IF SQL%NOTFOUND THEN
+        RAISE IDNotFoundEx;
+      END IF;
+    END removeId;
+
+	PROCEDURE removeName(nzw zespoly.nazwa%TYPE) IS
+	BEGIN
+      DELETE FROM zespoly WHERE nazwa = nzw;
+      
+      IF SQL%NOTFOUND THEN
+        RAISE nameNotFoundEx;
+      END IF;
+    END removeName;
+
+	PROCEDURE changeData(id zespoly.id_zesp%TYPE,
+				nzw zespoly.nazwa%TYPE,
+				adr zespoly.adres%TYPE) IS
+	BEGIN
+      UPDATE zespoly
+      SET nazwa = nzw,
+      	adres = adr
+      WHERE id_zesp = id;
+      
+      IF SQL%NOTFOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Nie zmieniono żadnego rekordu.');
+        RAISE IDNotFoundEx;
+      END IF;
+    END changeData;
+
+	FUNCTION getId(nzw zespoly.nazwa%TYPE)
+		RETURN zespoly.id_zesp%TYPE IS
+		ret zespoly.id_zesp%TYPE;
+	BEGIN
+      SELECT id_zesp INTO ret
+      	FROM zespoly WHERE nazwa = nzw;
+      RETURN ret;
+    EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+        RAISE nameNotFoundEx;
+    END getId;
+
+    FUNCTION getName(id zespoly.id_zesp%TYPE)
+		RETURN zespoly.nazwa%TYPE IS
+		ret zespoly.nazwa%TYPE;
+	BEGIN
+      SELECT nazwa INTO ret
+      	FROM zespoly WHERE id_zesp = id;
+      RETURN ret;
+    EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+        RAISE IDNotFoundEx;
+    END getName;
+
+    FUNCTION getAddress(id zespoly.id_zesp%TYPE)
+		RETURN zespoly.adres%TYPE IS
+		ret zespoly.adres%TYPE;
+	BEGIN
+      SELECT adres INTO ret
+      	FROM zespoly WHERE id_zesp = id;
+      RETURN ret;
+    EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+        RAISE IDNotFoundEx;
+      
+    END getAddress;
+END IntZespoly;
+
+
+BEGIN
+  -- IntZespoly.addTeam(20, 'Lorem ipsum', 'Somewhere');
+  DBMS_OUTPUT.PUT_LINE(IntZespoly.getId('Lorem ipsum'));
+EXCEPTION
+  WHEN IntZespoly.IDNotFoundEx THEN
+    DBMS_OUTPUT.PUT_LINE('Błąd: podane ID nie istnieje.');
+  WHEN IntZespoly.nameNotFoundEx THEN
+    DBMS_OUTPUT.PUT_LINE('Błąd: podana nazwa nie istnieje.');
+  WHEN IntZespoly.existingIdEx THEN
+    DBMS_OUTPUT.PUT_LINE('Błąd: zespół o takim ID już istnieje.');
 END;
