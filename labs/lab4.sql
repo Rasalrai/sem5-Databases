e415eedb6dc34b3fd19838ece011cbebe09621e7
Julia <39493975+Rasalrai@users.noreply.github.com>
2021-02-12 20:45:09 +0100

    finish lab 4

--- labs/lab4.sql
+++ labs/lab4.sql
@@ -107,4 +107,118 @@ CREATE OR REPLACE VIEW Szefowie
       FROM pracownicy
       GROUP BY id_szefa) pp ON ps.id_prac = pp.id_szefa;
 
--- 5 in progress
+CREATE OR REPLACE TRIGGER usun_podwladnych_szefa
+	INSTEAD OF DELETE ON szefowie
+	FOR EACH ROW
+DECLARE
+  CURSOR subs(sz pracownicy.nazwisko%TYPE) IS
+	SELECT id_prac, nazwisko FROM pracownicy WHERE nazwisko = sz FOR UPDATE;
+
+  temp NUMBER;
+BEGIN
+  FOR empl IN subs(:OLD.szef) LOOP
+    
+    SELECT podwladni INTO temp
+    FROM pracownicy ps LEFT JOIN (SELECT id_szefa, COUNT(*) as podwladni
+      FROM pracownicy
+      GROUP BY id_szefa) pp ON ps.id_prac = pp.id_szefa
+    WHERE ps.nazwisko = empl.nazwisko;
+    
+    IF temp IS NOT NULL THEN
+      RAISE_APPLICATION_ERROR(20001, 'Jeden z podwładnych usuwanego pracownika jest szefem innych pracowników. Usuwanie anulowane!'); 
+    END IF;
+  END LOOP;
+    -- if reaches here, safe to remove
+    
+  FOR empl IN subs(:OLD.szef) LOOP
+    DELETE FROM pracownicy WHERE id_prac = empl.id_prac;
+  END LOOP;
+    DELETE FROM pracownicy WHERE nazwisko = :OLD.szef;
+END;
+
+DELETE FROM szefowie WHERE szef = 'WEGLARZ';
+-- ORA-20001
+
+DELETE FROM szefowie WHERE szef = 'KONOPKA';
+-- OK
+
+
+-- zad 6
+
+ALTER TABLE zespoly
+ADD liczba_pracownikow NUMBER(6);
+
+UPDATE zespoly z
+SET liczba_pracownikow = (SELECT COUNT(*) FROM pracownicy WHERE id_zesp = z.id_zesp);
+
+CREATE OR REPLACE TRIGGER czlonkowie_zespolow
+  BEFORE DELETE OR INSERT OR UPDATE ON pracownicy
+  FOR EACH ROW
+BEGIN
+  CASE
+    WHEN INSERTING THEN
+      -- add employee
+      UPDATE zespoly SET liczba_pracownikow = liczba_pracownikow+1 WHERE id_zesp = :NEW.id_zesp;
+    WHEN DELETING THEN
+      -- remove employee
+      UPDATE zespoly SET liczba_pracownikow = liczba_pracownikow-1 WHERE id_zesp = :OLD.id_zesp;
+    WHEN UPDATING THEN
+      -- remove and add employee
+      UPDATE zespoly SET liczba_pracownikow = liczba_pracownikow+1 WHERE id_zesp = :NEW.id_zesp;
+      UPDATE zespoly SET liczba_pracownikow = liczba_pracownikow-1 WHERE id_zesp = :OLD.id_zesp;
+  END CASE;
+END;
+
+
+INSERT INTO Pracownicy(id_prac, nazwisko, id_zesp, id_szefa) VALUES(550,'NOWY PRACOWNIK', 40, 120);
+UPDATE Pracownicy SET id_zesp = 50 WHERE id_zesp = 40;
+DELETE FROM pracownicy WHERE id_prac = 550;
+
+
+-- zad 7
+
+ALTER TABLE pracownicy DROP CONSTRAINT fk_id_szefa;
+ALTER TABLE pracownicy ADD CONSTRAINT fk_id_szefa FOREIGN KEY(id_szefa) REFERENCES pracownicy(id_prac) ON DELETE CASCADE;
+
+CREATE OR REPLACE TRIGGER usun_prac
+  AFTER DELETE ON pracownicy
+  FOR EACH ROW
+BEGIN
+  DBMS_OUTPUT.PUT_LINE(:OLD.nazwisko);
+END;
+
+DELETE FROM pracownicy WHERE nazwisko = 'MORZY';
+-- Matysiak, Kowalski, Morzy
+ROLLBACK;
+
+CREATE OR REPLACE TRIGGER usun_prac
+  BEFORE DELETE ON pracownicy
+  FOR EACH ROW
+BEGIN
+  DBMS_OUTPUT.PUT_LINE(:OLD.nazwisko);
+END;
+
+DELETE FROM pracownicy WHERE nazwisko = 'MORZY';
+-- Morzy, Matysiak, Kowalski
+
+
+-- zad 8
+
+ALTER TABLE pracownicy DISABLE ALL TRIGGERS;
+
+SELECT * FROM user_triggers WHERE table_name = 'PRACOWNICY';
+
+alter trigger usun_podwladnych_szefa disable;
+
+
+-- zad 9
+
+SELECT * FROM user_triggers WHERE table_name in ('PRACOWNICY', 'ZESPOLY');
+
+DROP TRIGGER czlonkowie_zespolow;
+DROP TRIGGER logujoperacje;
+DROP TRIGGER pokazplace;
+DROP TRIGGER usun_podwladnych;
+DROP TRIGGER usun_prac;
+DROP TRIGGER uzupelnijid;
+DROP TRIGGER uzupelnijplace;
